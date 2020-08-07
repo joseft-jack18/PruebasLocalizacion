669390487519d9a02726e0e26b1df0fa789fe895
Harold Raymundo PACHA MAMANI <hrpacha@upt.pe>
2020-08-07 09:00:09 -0500

    Photo 100%

--- app/src/main/java/com/example/pruebaslocalizacion/PhotoActivity.kt
+++ app/src/main/java/com/example/pruebaslocalizacion/PhotoActivity.kt
@@ -1,5 +1,12 @@
 package com.example.pruebaslocalizacion
 
+import android.Manifest
+import android.content.ContentValues
+import android.content.pm.PackageManager
+import android.os.Build
+import android.provider.MediaStore
+import android.widget.Toast
+
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
@@ -12,6 +19,12 @@ import com.android.volley.toolbox.Volley
 import java.io.IOException
 
 class PhotoActivity : AppCompatActivity() {
+    private val PERMISSION_CODE = 1000;
+    private val IMAGE_CAPTURE_CODE = 1001
+    var image_uri: Uri? = null
+
+    private lateinit var capture_btn: Button
+
     private lateinit var imageView: ImageView
     private lateinit var imageButton: Button
     private lateinit var sendButton: Button
@@ -26,6 +39,31 @@ class PhotoActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo)
 
+        capture_btn = findViewById(R.id.capture_btn)
+
+        capture_btn.setOnClickListener {
+            //if system os is Marshmallow or Above, we need to request runtime permission
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_DENIED ||
+                    checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_DENIED){
+                    //permission was not enabled
+                    val permission = arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    //show popup to request permission
+                    requestPermissions(permission, PERMISSION_CODE)
+                }
+                else{
+                    //permission already granted
+                    openCamera()
+                }
+            }
+            else{
+                //system os is < marshmallow
+                openCamera()
+            }
+        }
+        //--
         imageView = findViewById(R.id.imageView)
 
         imageButton = findViewById(R.id.imageButton)
@@ -35,6 +73,34 @@ class PhotoActivity : AppCompatActivity() {
         sendButton = findViewById(R.id.sendButton)
         sendButton.setOnClickListener {
             uploadImage()
+        }
+    }
+
+    private fun openCamera() {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.TITLE, "Nueva Foto")
+        values.put(MediaStore.Images.Media.DESCRIPTION, "Desde la Camara")
+        image_uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+        //camera intent
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, image_uri)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        //called when user presses ALLOW or DENY from Permission Request Popup
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                    PackageManager.PERMISSION_GRANTED){
+                    //permission from popup was granted
+                    openCamera()
+                }
+                else{
+                    //permission from popup was denied
+                    Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 
@@ -80,7 +146,11 @@ class PhotoActivity : AppCompatActivity() {
                 imageView.setImageURI(uri)
                 createImageData(uri)
             }
+        }else if (resultCode == Activity.RESULT_OK){
+            //set image captured to image view
+            imageView.setImageURI(image_uri)
         }
+
         super.onActivityResult(requestCode, resultCode, data)
     }
 }
